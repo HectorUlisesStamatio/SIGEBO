ce3166a12e323190c356342d460196be5b9f92d8
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2023-03-08 23:30:51 -0600

    [UPDATE] commets

--- sigebo/src/main/resources/static/js/main.js
+++ sigebo/src/main/resources/static/js/main.js
@@ -1,12 +1,9 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SWIPER POPULAR ===============*/
 let swiperPopular = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursor: true,
@@ -20,45 +17,34 @@ let swiperPopular = new Swiper(".popular__container", {
     },
 })
 
-/*=============== VALUE ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.value__accordion-item')
 
-// 1. Select each item
 accordionItems.forEach((item) =>{
     const accordionHeader = item.querySelector('.value__accordion-header')
 
-    // 2. Select each header click
     accordionHeader.addEventListener('click', () =>{
-        // 7. Create the tag
+
         const openItem = document.querySelector('.accordion-open')
         
-        // 5. Call toggle item function
         toggleItem(item)
 
-        // 8. Check if the class exists
         if(openItem && openItem!== item){
             toggleItem(openItem)
         }
     })
 })
 
-// 3. Create a constant type function
 const toggleItem = (item) =>{
-    // 3.1 Create the tag
     const accordionContent = item.querySelector('.value__accordion-content')
-
-    // 6. If there is another element that contains the class accordion-open remove its class
     if(item.classList.contains('accordion-open')){
         accordionContent.removeAttribute('style')
         item.classList.remove('accordion-open')
     }else{
-        // 4. Add the maximum height of the content
         accordionContent.style.height = accordionContent.scrollHeight + 'px'
         item.classList.add('accordion-open')
     }
 }
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -82,11 +68,9 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
     try{
         const scrollUp = document.getElementById('scroll-up');
-        // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
         if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
     }catch(e){
 
@@ -97,7 +81,6 @@ window.addEventListener('scroll', scrollUp)
 
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
